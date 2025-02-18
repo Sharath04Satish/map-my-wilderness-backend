62f550fa38e10053c3d1722860d4586f13b1be36
Sharath Satish <sharath04satish@gmail.com>
2025-02-18 13:22:31 -0700

    Updated insert logic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const axios = require("axios");
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@primary-cluster.vjeq4.mongodb.net/?retryWrites=true&w=majority&appName=primary-cluster`;
+const uri = `${process.env.MONGO_DB_URI}`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -15,8 +15,16 @@ const client = new MongoClient(uri, {
 async function run(data) {
     try {
         await client.connect();
-        console.log("Pinged mongo-db server");
-        console.log(data[4]);
+        console.log("Connected to database");
+        const database = client.db(`${process.env.MONGO_DB_NAME}`);
+        const collection = database.collection(`${process.env.MONGO_DB_COLLECTION_1}`);
+
+        const del_result = await collection.deleteMany({});
+        if (del_result?.acknowledged === true) {
+            const options = { ordered: true };
+            const result = await collection.insertMany(data, options);
+            console.log(`${result.insertedCount} documents were inserted`);
+        }
     } finally {
         await client.close();
     }
@@ -29,7 +37,7 @@ axios.get(`https://developer.nps.gov/api/v1/parks?limit=${responseLimit}`, {
     }
 }).then((response) => {
     if (response) {
-        if (response.status == 200) {
+        if (response.status === 200) {
             if (response?.data?.data) {
                 handleResponseData(response.data.data);
             }
