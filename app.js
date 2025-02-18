f805191615676d7089d16002bcc527b2fcd27df3
Sharath Satish <sharath04satish@gmail.com>
2025-02-18 09:44:46 -0700

    Added mongo-db client

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const axios = require("axios");
 
-const responseLimit = 500;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@primary-cluster.vjeq4.mongodb.net/?retryWrites=true&w=majority&appName=primary-cluster`;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run(data) {
+    try {
+        await client.connect();
+        console.log("Pinged mongo-db server");
+        console.log(data[4]);
+    } finally {
+        await client.close();
+    }
+}
+
+const responseLimit = 500;
 axios.get(`https://developer.nps.gov/api/v1/parks?limit=${responseLimit}`, {
     headers: {
         "x-api-key": `${process.env.API_KEY}`
@@ -10,7 +30,7 @@ axios.get(`https://developer.nps.gov/api/v1/parks?limit=${responseLimit}`, {
 }).then((response) => {
     if (response) {
         if (response.status == 200) {
-            if (response.data && response.data.data) {
+            if (response?.data?.data) {
                 handleResponseData(response.data.data);
             }
         }
@@ -35,5 +55,7 @@ const handleResponseData = (data) => {
             responseData.push(trimmedSite);
         }
     });
+
+    run(responseData).catch(console.dir);
 }
 
