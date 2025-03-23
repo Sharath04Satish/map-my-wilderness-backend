ee38b6c6f85b0494c53a265b5d58ef257db85a54
Sharath Satish <sharath04satish@gmail.com>
2025-03-22 23:36:56 -0600

    Added logic to validate jwt

--- authn/tokenAuth.js
+++ authn/tokenAuth.js
@@ -1,28 +1,47 @@
-import * as jose from 'jose';
+import { SignJWT, jwtVerify } from "jose";
 import { publicKey, privateKey } from "./getKeyPair.js";
+import dotenv from 'dotenv';
 
-const alg = "PS256";
+dotenv.config({ path: "../.env" });
 
-export const getJwt = async (userName, userEmail) => {
-    return await new jose.SignJWT({ 'sub': userEmail, 'name': userName })
-        .setProtectedHeader({ alg: alg, typ: 'JWT' })
+export const generateIdToken = async (userName, userEmail) => {
+    return await new SignJWT({ 'sub': userEmail, 'name': userName })
+        .setProtectedHeader({ alg: `${process.env.PUBLIC_KEY_ALGORITHM}`, typ: 'JWT' })
         .setIssuedAt()
-        .setIssuer('map-my-wilderness-auth')
-        .setAudience('map-my-wilderness-api')
-        .setExpirationTime('1h')
+        .setIssuer(`${process.env.JWT_ISSUER}`)
+        .setAudience(`${process.env.JWT_AUDIENCE}`)
+        .setExpirationTime(`${process.env.JWT_EXPIRATION}`)
         .sign(privateKey)
-}
+};
 
-const jwt = await getJwt("Sharath", "sharaths1998@gmail.com")
+export const verifyIdToken = async (token) => {
+    try {
+        const { payload, protectedHeader } = await jwtVerify(token, publicKey, {
+            issuer: `${process.env.JWT_ISSUER}`,
+            audience: `${process.env.JWT_AUDIENCE}`
+        });
 
-try {
-    const { payload, protectedHeader } = await jose.jwtVerify(jwt, publicKey, {
-        issuer: 'map-my-wilderness-auth',
-        audience: 'map-my-wilderness-api'
-    });
+        if (protectedHeader) {
+            if (protectedHeader.alg === `${process.env.PUBLIC_KEY_ALGORITHM}` && protectedHeader.typ === "JWT") {
+                if (payload.aud === `${process.env.JWT_AUDIENCE}` && payload.iss === `${process.env.JWT_ISSUER}`) {
+                    const currentDT = Date.now();
+                    const issuedAtDT = new Date(parseInt(payload.iat) * 1000);
+                    const expirationDT = new Date(parseInt(payload.exp) * 1000);
 
-    console.log(payload);
-    console.log(protectedHeader);
-} catch (ex) {
-    console.log(ex);
-}
+                    if (currentDT >= issuedAtDT && currentDT <= expirationDT) {
+                        // Validate user from database.
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    } catch (ex) {
+        console.error(ex);
+    }
+};
+
+const token = await generateIdToken("Sharath Satish", "sharaths1998@gmail.com");
+const isValidToken = await verifyIdToken(token);
+console.log(isValidToken);
